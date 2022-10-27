bcc3e21eec3b6439766faf54775c37ce047369db
Alex Smith <smithalexlee30@gmail.com>
2022-10-26 23:49:04 -0500

    created loss acc script and png

--- homework/other/plot.py
+++ homework/other/plot.py
@@ -1,12 +1,20 @@
 from pandas import read_csv
 from matplotlib import pyplot
 
-dataPath = 'resnet34_loss_acc.csv'
+dataPath = 'homework/other/resnet34_loss_acc.csv'
+pngPath = 'homework/4_resnet34_loss_acc.png'
 
 data = read_csv(dataPath)
 
-fig, ax = pyplot.subplots(1, 2, figsize=(18, 6), dpi=80) # Create a 1 by 2 plot grid.
+fig, ax = pyplot.subplots(2, 1, figsize=(16, 6), dpi=80, sharex=True)
 
-ax[0].plot(data['batch'], data['loss'])
+step = 15
+ax[0].plot(data['batch'][::step], data['loss'][::step])
+ax[0].set_title('ResNet34 (1 epoch, batch size = 256)')
+ax[0].set_ylabel('Loss')
 
-ax[1].plot(data['batch'], data['acc'])
+ax[1].plot(data['batch'][::step], data['accuracy'][::step])
+ax[1].set_xlabel('Batch #')
+ax[1].set_ylabel('Accuracy')
+
+fig.savefig(pngPath)
