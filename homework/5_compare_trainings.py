3cd0e9a54ee3c1b0b68a987a1b4e3ab85293e742
Alex Smith <smithalexlee30@gmail.com>
2022-11-01 21:52:25 -0500

    ready to run hw5 on thedagpu

---
+++ homework/5_compare_trainings.py
@@ -0,0 +1,14 @@
+from os import system, remove, path
+
+if __name__ == "__main__":
+    OUT_FILENAME = "5_compare_trainings_out.txt"
+    
+    if path.exists(OUT_FILENAME):
+        remove(OUT_FILENAME)
+    
+    for numParallelThreads in [2, 8, 32, 128]:
+        for prefetchBufferSize in [2, 4, 6, 8]:
+            system(f"python3 5_train_resnet34.py {numParallelThreads} {prefetchBufferSize}")
+            
+        with open(OUT_FILENAME, 'a') as outfile:
+            outfile.write('~~~~~~~~~~~~~~~')
